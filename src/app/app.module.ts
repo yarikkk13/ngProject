f603a6a19c4f8a40c1c4c77f2ff99330872728d3
yarikkk13 <yaroslav.mahas@gmail.com>
2021-09-17 15:55:21 +0300

    intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageComponent } from './components/page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    FooterComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/Course.ts
@@ -0,0 +1,7 @@
+export interface ICourse {
+  id: number;
+  title: string;
+  creationTime: string;
+  duration: number;
+  description: string;
+}
