20c436cde960a59fed4453643b3102145e0ca721
yarikkk13 <yaroslav.mahas@gmail.com>
2021-10-09 10:55:08 +0300

    header added data modifiers

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,11 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  videoCurse:string = 'VIDEO COURSE';
+  public videoCurse: string = 'VIDEO COURSE';
 
-  userLogin:string = 'User login';
+  public userLogin: string = 'User login';
 
-  logOff:string = 'Log off';
+  public logOff: string = 'Log off';
 
   ngOnInit(): void {
   }
