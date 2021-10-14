10a66f6d1db016f969d48df23f9ed6535f7f4add
yarikkk13 <yaroslav.mahas@gmail.com>
2021-10-14 19:46:47 +0300

    methods in courses, authService

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@angular/core';
-import { courses } from "../mockArrays/mockCourses";
+import {Injectable} from '@angular/core';
+import {courses} from "../mockArrays/mockCourses";
+import {ICourse} from "../models/Course";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
-  getCourses() {
+  getCourses(): ICourse[] {
     return courses;
+  }
+
+  getCourseById(id: string): ICourse[] {
+    return courses.filter(course => course.id == +id)
+  }
+
+  createCourse(id: number, title: string, creationTime: Date, duration: number, description: string, topRated: boolean): ICourse[] {
+    const newCourse: ICourse = {id, title, creationTime, duration, description, topRated}
+    let list:ICourse[] = courses;
+    list.push(newCourse)
+    return list
+  }
+
+  updateCourse(course: ICourse, ...args: []): ICourse {
+    return course
+  }
+
+  removeCourse(courses: ICourse): any {
+    return null
   }
 }
