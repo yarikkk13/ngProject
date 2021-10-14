a98fc66e3f41785908f8af037d0e571d2cd94817
yarikkk13 <yaroslav.mahas@gmail.com>
2021-10-14 18:04:59 +0300

    creationDateDirectiveDone

--- src/app/directives/creation-date.directive.ts
+++ src/app/directives/creation-date.directive.ts
@@ -1,18 +1,20 @@
-import {Directive, ElementRef, Input } from '@angular/core';
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 import {ICourse} from "../models/Course";
 
 @Directive({
   selector: '[appCreationDate]'
 })
-export class CreationDateDirective {
+export class CreationDateDirective implements OnInit {
   @Input()
   directivesCourse: ICourse;
 
   constructor(private elementRef: ElementRef) {
-    const courseDate:number = this.directivesCourse?.creationTime.getTime();
-    const currentDate:number = Date.now();
-    const fourteenDays:number = 1209600000;
+  }
 
+  creationDateDirectiveFunc() {
+    const courseDate = this.directivesCourse?.creationTime.getTime();
+    const currentDate = Date.now();
+    const fourteenDays = 1209600000;
     if (courseDate < currentDate && courseDate >= currentDate - fourteenDays) {
       this.elementRef.nativeElement.style.border = "solid 1px green";
     } else if (courseDate > currentDate) {
@@ -20,4 +22,7 @@ export class CreationDateDirective {
     }
   }
 
+  ngOnInit() {
+    this.creationDateDirectiveFunc()
+  }
 }
