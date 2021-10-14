d37ddf33cdacf42fbf0599899c2d0bf8b5098dfa
yarikkk13 <yaroslav.mahas@gmail.com>
2021-10-14 20:01:24 +0300

    simple methods in authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  login(): any {
+    return null;
+  }
+
+  logOut(): any {
+    return null;
+  }
+
+  isAuthenticated(): boolean {
+    return false;
+  }
+
+  getUserInfo(): any {
+    return null;
+  }
 }
