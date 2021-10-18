b263eb4758d5a887fbfb31a669ea44f80a8bd564
yarikkk13 <yaroslav.mahas@gmail.com>
2021-10-18 16:06:37 +0300

    if Authenticated derective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CreationDateDirective } from './directives/creation-date.directive';
 import { OrderByPipe } from './pipes/order-by.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
 import { LoginComponent } from './components/login/login.component';
+import { IfAuthenticatedDirective } from './directives/if-authenticated.directive';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { LoginComponent } from './components/login/login.component';
     CreationDateDirective,
     OrderByPipe,
     FilterPipe,
-    LoginComponent
+    LoginComponent,
+    IfAuthenticatedDirective
   ],
   imports: [
     BrowserModule,
