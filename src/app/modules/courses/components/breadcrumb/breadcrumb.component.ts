90cf1fabcb53105db327da6874cceff4902bc7df
yarikkk13 <yaroslav.mahas@gmail.com>
2021-11-04 17:28:02 +0200

    breadcrumb start

--- src/app/modules/courses/components/breadcrumb/breadcrumb.component.ts
+++ src/app/modules/courses/components/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ICourse} from "../../../../models/Course";
 
 @Component({
   selector: 'app-breadcrumb',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class BreadcrumbComponent implements OnInit {
+
+  @Input()
+  course: ICourse;
 
   public courses: string = 'Courses';
 
