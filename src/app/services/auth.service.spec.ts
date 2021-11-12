0aa732b0dec29fb87580d3c33aa97f1d286c042a
yarikkk13 <yaroslav.mahas@gmail.com>
2021-11-12 14:09:34 +0200

    07

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import {AuthService} from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -12,5 +12,36 @@ describe('AuthService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('is logged in?', () => {
+    service.login('email', 'password')
+    const loggedIn = service.isAuthenticated()
+    expect(loggedIn).toBeTruthy()
+  });
+
+  it('check user email', () => {
+    service.login('email', 'password')
+    const email = localStorage.getItem('email');
+    expect(email).toBe('email')
+  });
+
+  it('check user password', () => {
+    service.login('email', 'password')
+    const password = localStorage.getItem('password');
+    expect(password).toBe('password')
+  });
+
+  it('is logged out?', () => {
+    service.login('email', 'password')
+    service.logOut()
+    const loggedIn = service.isAuthenticated()
+    expect(loggedIn).toBeFalse()
+  });
+
+  it('get user info', () => {
+    service.login('email', 'password')
+    const info = service.getUserInfo()
+    expect(info).toEqual({email: 'email', password: 'password'})
   });
 });
--- src/app/services/courses.service.spec.ts
+++ src/app/services/courses.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CoursesService } from './courses.service';
+import { courses } from "../mockArrays/mockCourses";
 
 describe('CoursesService', () => {
   let service: CoursesService;
+  let mockedCourses = courses;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,4 +15,41 @@ describe('CoursesService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getCourses', () => {
+    const testedCourses = service.getCourses()
+    expect(testedCourses).toEqual(mockedCourses);
+  });
+
+  it('courseById1', () => {
+    const testedCourse = service.getCourseById('1');
+    expect(testedCourse).toBe(mockedCourses[0])
+  });
+
+  it('courseById2', () => {
+    const testedCourse = service.getCourseById('2');
+    expect(testedCourse).toBe(mockedCourses[1])
+  });
+
+  it('added', () => {
+    service.createCourse({
+      id: 3,
+      title: 'third title course',
+      creationTime: new Date(2020, 10, 6),
+      duration: 99,
+      description: 'Examine a wide variety of technologies that have influenced our society significantly. Analyze the contribution\n' +
+        '        these technologies make to society, associated ethical dilemmas, and critique their value to the individual and\n' +
+        '        society.Initially, course descriptions are written when a new program is being developed or when developing a\n' +
+        '        new course.',
+      topRated: false
+    });
+    expect(mockedCourses.length).toBe(3)
+  });
+
+  it('remove', () => {
+    service.removeCourse('2');
+    const notFoundCourse = service.getCourseById('2')
+    expect(notFoundCourse).toBe(undefined)
+  });
+
 });
