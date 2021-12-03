21500cd458092fa4887f67c426e5b2da1a9a7049
yarikkk13 <yaroslav.mahas@gmail.com>
2021-12-03 16:04:38 +0200

    searching behavior

--- src/app/modules/courses/components/section/section.component.ts
+++ src/app/modules/courses/components/section/section.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 
 import {ICourse} from "../../../../models/Course";
 import {CoursesService} from "../../../../services/courses.service";
+import {Observable, Subject} from "rxjs";
+import {debounceTime, distinctUntilChanged, filter, switchMap} from "rxjs/operators";
+import {verifySupportedTypeScriptVersion} from "@angular/compiler-cli/src/typescript_support";
 
 @Component({
   selector: 'app-section',
@@ -13,13 +16,25 @@ export class SectionComponent implements OnInit {
 
   public courses: ICourse[];
 
-  public search: string = 'Search';
   public add: string = 'Add course';
   public load: string = 'LOAD MORE';
   public noData: string = 'No data.Feel free to add new course';
   public searchText: string = '';
+
+  private searchText$ = new Subject<string>();
+
   public start: number = 0;
   public count: number = 4;
+
+  getValue(event: Event): string {
+    // if ((event.target as HTMLInputElement).value.length > 3)
+      return (event.target as HTMLInputElement).value;
+    // else return ''
+  }
+
+  search(text: string) {
+    this.searchText$.next(text);
+  }
 
   constructor(private courseService: CoursesService) {
   }
@@ -48,11 +63,10 @@ export class SectionComponent implements OnInit {
   //   }
   //   return this.courses = this.courses?.filter(course => course.title.includes(searchText))
   // };
-
-  public showTheSearching(searchText: string): void {
-    this.courseService.getCoursesByFragment(searchText)
-      .subscribe(value => this.courses = value)
-  };
+  // public showTheSearching(searchText: string): void {
+  //   this.courseService.getCoursesByFragment(searchText)
+  //     .subscribe(value => this.courses = value)
+  // };
 
 
   public showMore(): void {
@@ -66,6 +80,14 @@ export class SectionComponent implements OnInit {
     this.courseService.getCourses(this.start, this.count)
       .subscribe(value => this.courses = value)
     console.log('init') //lifecycle hooks to understand the ordering
+
+    this.searchText$.pipe(
+      filter(value => value.length >= 3),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(text =>
+        this.courseService.getCoursesByFragment(text))
+    ).subscribe(value => this.courses = value);
   };
 
 }
